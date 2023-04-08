20962769219d10d796bef143c24699c2d83a82e1
BigPhatNerd <wilsonhorrell@gmail.com>
2023-04-08 13:59:41 -0500

    aboveFreezing

--- aboveFreezing.js
+++ aboveFreezing.js
@@ -1,4 +1,9 @@
 function aboveFreezing(temperature){
+    // I am using console.log with an object {} to log the result of the
+    // temperature > 32 comparison. This is the same as console.log(temperature
+    // >
+    // 32) except the `aboveFreezing` property will be added to the log (kinda
+    //     like a label)
     console.log({ aboveFreezing: temperature > 32})
     return temperature > 32
 }
